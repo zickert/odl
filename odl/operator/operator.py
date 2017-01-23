16bdc73365fe50d551c7981cdc09e96e7044c73f
Holger Kohr <h.kohr@cwi.nl>
2017-01-23 22:31:35 +0100

    ENH: finish porting existing code to nD data containers

--- odl/operator/operator.py
+++ odl/operator/operator.py
@@ -1522,6 +1522,7 @@ class OperatorLeftScalarMult(Operator):
     def _call(self, x, out=None):
         """Implement ``self(x[, out])``."""
         if out is None:
+            print(self.operator(x))
             return self.scalar * self.operator(x)
         else:
             self.operator(x, out=out)
