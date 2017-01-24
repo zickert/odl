46bbf171f574ae8505442622239e0fd43c931fa8
Holger Kohr <h.kohr@cwi.nl>
2017-01-24 18:48:08 +0100

    MAINT: fixes to deformation operator and others

--- odl/discr/grid.py
+++ odl/discr/grid.py
@@ -713,7 +713,7 @@ class RectGrid(Set):
         Parameters
         ----------
         order : {'C', 'F'}, optional
-            Axis ordering in the resulting point array
+            Axis ordering in the resulting point array.
 
         Returns
         -------
