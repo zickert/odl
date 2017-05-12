e03616cf5f53bae87acc208e8f7ecc6a56fd17d7
Holger Kohr <h.kohr@cwi.nl>
2017-05-12 16:45:14 +0200

    BUG: fix some issues related to empty shapes

--- odl/discr/grid.py
+++ odl/discr/grid.py
@@ -464,7 +464,7 @@ class RectGrid(Set):
         if other is self:
             return True
 
-        return (isinstance(other, RectGrid) and
+        return (type(other) is type(self) and
                 self.ndim == other.ndim and
                 self.shape == other.shape and
                 all(np.allclose(vec_s, vec_o, atol=atol, rtol=0.0)
@@ -478,7 +478,7 @@ class RectGrid(Set):
         if other is self:
             return True
 
-        return (isinstance(other, RectGrid) and
+        return (type(other) is type(self) and
                 self.shape == other.shape and
                 all(np.array_equal(vec_s, vec_o)
                     for (vec_s, vec_o) in zip(self.coord_vectors,
