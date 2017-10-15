46001bab9f5ad0b7e607b0231fe625cdca5f158f
Gustav Zickert <zickert@kth.se>
2017-10-15 17:03:02 +0200

    WIP: clean up 2d example

--- examples/tomo/electron_tomo_2d.py
+++ examples/tomo/electron_tomo_2d.py
@@ -10,34 +10,6 @@ import numpy as np
 import odl
 from intensity_op import IntensityOperator
 
-
-def pupil_function(x, **kwargs):
-    """Indicator function for the disc-shaped aperture, a.k.a. pupil function.
-
-
-    Notes
-    -----
-    The pupil fuction is defined as
-
-    .. math::
-
-        A_\\Sigma(\\xi) = \\begin{cases}1,& \\Big\\|\\frac{f}{k}\\xi\\Big\\|
-        \\leq r\\\\ 0,& \\Big\\|\\frac{f}{k}\\xi\\Big\\| > r \\end{cases}
-
-    where :math:`\kappa` is the wave number of the incoming electron wave,
-    :math:`r` is the radius of the aperture and :math:`f` is the focal length
-    of the principal lens.
-    """
-    aper_rad = kwargs.pop('aper_rad')
-    focal_length = kwargs.pop('focal_length')
-    wave_number = kwargs.pop('wave_number')
-    scaled_rad = aper_rad * wave_number / focal_length
-
-    norm_sq = np.sum(xi ** 2 for xi in x[1:])
-
-    return norm_sq <= scaled_rad ** 2
-
-
 def optics_imperfections(x, **kwargs):
     """Function encoding the phase shifts due to optics imperfections.
 
@@ -71,33 +43,6 @@ def optics_imperfections(x, **kwargs):
                                                     wave_number ** 2 - 2 *
                                                     defocus)
     result = np.exp(1j * result)
-
-    return result
-
-
-def modulation_transfer_function(x, **kwargs):
-    """Function that characterizes the detector response.
-
-
-    Notes
-    -----
-    The modulation transfer function is defined as
-
-    .. math::
-
-        MTF(\\xi) = \\frac{a}{1 + \\alpha\\|\\xi\\|^2} +
-        \\frac{b}{1+\\beta\\|\\xi\\|^2}+c
-
-    where :math:`a,b,c,\\alpha` and :math:`\\beta` are real parameters."""
-    a = kwargs.pop('mtf_a')
-    b = kwargs.pop('mtf_b')
-    c = kwargs.pop('mtf_c')
-    alpha = kwargs.pop('mtf_alpha')
-    beta = kwargs.pop('mtf_beta')
-
-    norm_sq = np.sum(xi ** 2 for xi in x[1:]) / M**2
-
-    result = a / (1 + alpha * norm_sq) + b / (1 + beta * norm_sq) + c
 
     return result
 
@@ -137,7 +82,7 @@ ray_trafo = odl.tomo.RayTransform(reco_space, geometry)
 
 # Choose constant before ray_trafo so that the result is small enough for a
 # linearisation of the exponential to make sense.
-scattering_op = ray_trafo
+scattering_op = ray_trafo.range.one() + 0.01j * ray_trafo
 
 ft_ctf = odl.trafos.FourierTransform(scattering_op.range, axes=1)
 
@@ -145,11 +90,14 @@ optics_imperf = ft_ctf.range.element(optics_imperfections,
                                      wave_number=wave_number,
                                      spherical_abe=spherical_abe,
                                      defocus=defocus)
-ctf = 2 * np.pi * optics_imperf
+
+# Leave out pupil-function since it has no effect
+ctf = optics_imperf
 optics_op = ft_ctf.inverse * ctf * ft_ctf
 
 intens_op = IntensityOperator(optics_op.range)
 
+# Leave out detector operator for simplicity
 forward_op = intens_op * optics_op * scattering_op
 #forward_op = optics_op * scattering_op
 
@@ -157,14 +105,13 @@ phantom = (1+1j) * odl.phantom.shepp_logan(reco_space, modified=True)
 
 data = forward_op(phantom)
 
-reco = 1.01*phantom
+reco = reco_space.zero()
 callback = (odl.solvers.CallbackPrintIteration() &
             odl.solvers.CallbackShow())
 odl.solvers.conjugate_gradient_normal(forward_op, reco, data,
-                                      niter=4, callback=callback)
+                                      niter=10, callback=callback)
+# non-linear cg must be adapted to complex case
 #func = odl.solvers.L2NormSquared(data.space).translated(data) * forward_op
-#
-#
 #odl.solvers.conjugate_gradient_nonlinear(func, reco, line_search=1e0, callback=callback,
 #                                         nreset=50)
 
