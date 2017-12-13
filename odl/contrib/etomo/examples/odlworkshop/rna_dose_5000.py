4f970b8fdf15040aff6029834ee56dedcacb7d8f
Gustav Zickert <zickert@kth.se>
2017-12-13 14:30:35 +0100

    clean up reconstruction from Simulator

--- odl/contrib/etomo/examples/odlworkshop/rna_dose_5000.py
+++ odl/contrib/etomo/examples/odlworkshop/rna_dose_5000.py
@@ -7,31 +7,17 @@ import numpy as np
 import os
 import odl
 from odl.contrib import etomo
-
-
 from odl.contrib.mrc import FileReaderMRC
 
 # Read phantom and data.
 dir_path = os.path.abspath('/home/zickert/TEM_reco_project/Data/One_particle_RNA/odlworkshop/dose_5000')
 file_path_phantom = os.path.join(dir_path, 'rna_phantom.mrc')
-file_path_phantom_abs = os.path.join(dir_path, 'rna_phantom_abs.mrc')
-file_path_map = os.path.join(dir_path, '1I3Q_map.mrc')
-file_path_map_abs = os.path.join(dir_path, '1I3Q_abs_map.mrc')
 file_path_tiltseries = os.path.join(dir_path, 'tiltseries.mrc')
-file_path_tiltseries_nonoise = os.path.join(dir_path, 'tiltseries_nonoise.mrc')
 
 with FileReaderMRC(file_path_phantom) as phantom_reader:
     phantom_header, phantom_asarray = phantom_reader.read()
-#with FileReaderMRC(file_path_phantom_abs) as phantom_abs_reader:
-#    phantom_abs_header, phantom_abs_asarray = phantom_abs_reader.read()
-#with FileReaderMRC(file_path_map) as map_reader:
-#    map_header, map_asarray = map_reader.read()
-#with FileReaderMRC(file_path_map_abs) as map_abs_reader:
-#    map_abs_header, map_abs_asarray = map_abs_reader.read()
 with FileReaderMRC(file_path_tiltseries) as tiltseries_reader:
     tiltseries_header, data_asarray = tiltseries_reader.read()
-with FileReaderMRC(file_path_tiltseries_nonoise) as tiltseries_nonoise_reader:
-    tiltseries_nonoise_header, data_nonoise_asarray = tiltseries_nonoise_reader.read()
 
 # The reconstruction space will be rescaled according to rescale_factor in
 # order to avoid numerical issues related to having a very small reco space.
@@ -47,18 +33,13 @@ wave_number = 2 * np.pi / wave_length
 
 sigma = e_mass * e_charge / (wave_number * planck_bar ** 2)
 
-
-# Set ratio to 0.1 to match with Leonies data
 abs_phase_ratio = 0.1
 obj_magnitude = 0.5*sigma / rescale_factor
 regpar = 2e3
+gamma_H1 = 0.9
 num_angles = 61
 num_angles_per_block = 1
 num_cycles = 3
-
-# total_dose = 5000 * 1e18  # total electron dose per m^2
-# dose_per_img = total_dose / 61
-# gain = 80  # average nr of digital counts per incident electron
 
 # Define sample diameter and height. We take flat sample
 sample_diam = 1200e-9  # m
@@ -114,24 +95,17 @@ mask = reco_space.element(etomo.spherical_mask,
 
 # Define forward operator as a composition
 forward_op = imageFormation_op * ray_trafo
-
 phantom = reco_space.element(phantom_asarray)
-
-#phantom_abs = reco_space.element(phantom_abs_asarray)
-
 
 # remove background
 bg_cst = np.min(phantom)
 phantom -= bg_cst
 
 # Create data by calling the forward operator on the phantom
-data = forward_op(phantom)
+data_from_this_model = forward_op(phantom)
 
-true_data = forward_op.range.element(np.transpose(data_asarray,
-                                                  (2, 0, 1)))
-
-
-reco = ray_trafo.domain.zero()
+# Make  a ODL discretized function of the MRC data
+data = forward_op.range.element(np.transpose(data_asarray, (2, 0, 1)))
 
 # Plot phantom and data
 #phantom.show(coords=[0, None, None])
@@ -142,39 +116,21 @@ reco = ray_trafo.domain.zero()
 #plt.colorbar()
 #true_data.show(coords=[0, [-2e1, 2e1], [-2e1, 2e1]])
 
-#background = bg_cst * ray_trafo.domain.one()
-#
-#bg_abs_cst = np.min(phantom_abs)
-#bg_ratio = bg_abs_cst/bg_cst
-#
-#imageFormation_op_bg = make_imageFormationOp(ray_trafo.range, 
-#                                          wave_number, spherical_abe, defocus,
-#                                          det_size, M, rescale_ctf=False,
-#                                          obj_magnitude=obj_magnitude,abs_phase_ratio=bg_ratio,
-#                                          dose_per_img=dose_per_img, gain=gain,
-#                                          det_area=det_area)
-#
-#forward_op_bg = imageFormation_op_bg * ray_trafo 
-#
-#data_bg = forward_op_bg(background)
-#data_bg.show(coords=[0,None,None])
-
-# Correct for diffrent pathlenght of the electrons through the buffer  
-true_data_bc = etomo.buffer_correction(true_data)
-data_bc = etomo.buffer_correction(data)
+# Correct for diffrent pathlenght of the electrons through the buffer
+data = etomo.buffer_correction(data)
+data_from_this_model = etomo.buffer_correction(data_from_this_model)
 
 # Plot corrected data
 #data_bc.show(coords=[0, [-2e1, 2e1], [-2e1, 2e1]])
 #true_data_bc.show(coords=[0, [-2e1, 2e1], [-2e1, 2e1]])
 
-data_renormalized = true_data_bc * (np.mean(data.asarray()))
+# Renormalize data so that it matches "data_from_this_model"
+data *= np.mean(data_from_this_model.asarray())
 
-
-# %% TRY RECONSTRUCTION
-
+# %% RECONSTRUCTION
+reco = ray_trafo.domain.zero()
 callback = (odl.solvers.CallbackPrintIteration() &
             odl.solvers.CallbackShow())
-#callback = None
 
 kaczmarz_plan = etomo.make_kaczmarz_plan(num_angles,
                                          block_length=num_angles_per_block,
@@ -192,7 +148,7 @@ F_pre = odl.MultiplyOperator(mask, reco_space, reco_space)
 
 get_op = etomo.make_Op_blocks(kaczmarz_plan, ray_trafo, Op_pre=F_pre,
                               Op_post=F_post)
-get_data = etomo.make_data_blocks(data_renormalized, kaczmarz_plan)
+get_data = etomo.make_data_blocks(data, kaczmarz_plan)
 
 # Optional nonnegativity-constraint
 nonneg_projection = etomo.get_nonnegativity_projection(reco_space)
@@ -204,26 +160,24 @@ get_proj_op = etomo.make_Op_blocks(kaczmarz_plan, ray_trafo, Op_pre=F_pre,
 
 etomo.kaczmarz_SART_method(get_proj_op, reco, get_data, len(kaczmarz_plan),
                            regpar*obj_magnitude ** 2,
-                           imageFormationOp=F_post, gamma_H1=0.9, niter_CG=30,
-                           callback=callback, num_cycles=num_cycles,
-                           projection=nonneg_projection)
+                           imageFormationOp=F_post, gamma_H1=gamma_H1,
+                           niter_CG=30, callback=callback,
+                           num_cycles=num_cycles, projection=nonneg_projection)
 
 
 # Plot results
 #etomo.plot_3d_ortho_slices(phantom)
 #etomo.plot_3d_ortho_slices(reco)
 
-dose_5000_reco_fig = reco.show(coords = [None,None,0])
-dose_5000_reco_fig.savefig('dose_5000_reco')
+# Save planes of reco (orthogonal to x,y and z axes) 
+dose_5000_reco_fig_x = reco.show(title='rna_dose_5000_reco_x',
+                                 coords=[0, None, None])
+dose_5000_reco_fig_x.savefig('rna_dose_5000_reco_x')
 
-# %%
+dose_5000_reco_fig_y = reco.show(title='rna_dose_5000_reco_y',
+                                 coords=[None, 0, None])
+dose_5000_reco_fig_y.savefig('rna_dose_5000_reco_y')
 
-
-#map_slice = map_asarray[:,:,79]
-#map_abs_slice = map_abs_asarray[:,:,79]
-#                                
-#plt.imshow(map_slice)
-#plt.colorbar()
-#plt.figure()
-#plt.imshow(map_abs_slice)
-#plt.colorbar()                               
+dose_5000_reco_fig_z = reco.show(title='rna_dose_5000_reco_z',
+                                 coords=[None, None, 0])
+dose_5000_reco_fig_z.savefig('rna_dose_5000_reco_z')
