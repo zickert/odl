7b5df1698e6de45e15b97930c8ec0cc0baa2d996
Gustav Zickert <zickert@kth.se>
2018-02-11 17:06:09 +0100

    WIP: Add param sweep for pdhg_tv, balls

--- odl/contrib/etomo/examples/simulated/balls/pdhg_tv_lin/balls_dose_6000_param_sweep.py
+++ odl/contrib/etomo/examples/simulated/balls/pdhg_tv_lin/balls_dose_6000_param_sweep.py
@@ -117,17 +117,11 @@ data = etomo.buffer_correction(data, coords=[[0, 0.1], [0, 0.1]])
 
 data_from_this_model = etomo.buffer_correction(data_from_this_model)
 
-# Plot corrected data
-data_from_this_model.show(coords=[0, None, None])
-data_from_this_model_lin.show(coords=[0, None, None])
 
 # Renormalize data so that it matches "data_from_this_model"
-data *= np.mean(data_from_this_model_lin.asarray())
+#data *= np.mean(data_from_this_model_lin.asarray())
 
-nonlinearity = data_from_this_model - data_from_this_model_lin
-nonlinearity.show(coords=[0, None, None])
 
-(data-data_from_this_model_lin).show(coords = [0,None,None])
 
 
 # %%
@@ -139,50 +133,62 @@ nonlinearity.show(coords=[0, None, None])
 forward_op = lin_op
 
 
-# Initialize gradient operator
-gradient = odl.Gradient(reco_space)
+reg_param_list = [1e-3, 3e-3, 1e-2, 3e-2, 1e-1]
+step_param_list = [1e-4, 1e-3, 1e-2, 1e-1]
 
-# Column vector of two operators
-op = odl.BroadcastOperator(forward_op, gradient)
+reco_path = '/home/zickert/TEM_reco_project/Reconstructions/Simulated/Balls/dose_6000/pdhg_tv_pos_constr_LINEARIZED'
 
-# Do not use the g functional, set it to zero.
-g = odl.solvers.IndicatorNonnegativity(reco_space)
+for reg_param in reg_param_list:
+    for step_param in step_param_list:
 
-# Create functionals for the dual variable
-
-# l2-squared data matching
-l2_norm = odl.solvers.L2NormSquared(forward_op.range).translated(data)
-
-#
-reg_param = 0.015
-
-# Isotropic TV-regularization i.e. the l1-norm
-l1_norm = reg_param * odl.solvers.GroupL1Norm(gradient.range)
-
-# Combine functionals, order must correspond to the operator K
-f = odl.solvers.SeparableSum(l2_norm, l1_norm)
-
-# --- Select solver parameters and solve using PDHG --- #
-
-# Estimated operator norm, add 10 percent to ensure ||K||_2^2 * sigma * tau < 1
-op_norm = 1.1 * 0.067 # 1.1 * odl.power_method_opnorm(forward_op.derivative(reco_space.zero()))
-
-
-niter = 10000  # Number of iterations
-tau = 0.01 / op_norm  # Step size for the primal variable
-sigma = 0.01 / op_norm  # Step size for the dual variable
-
-
-
-# Choose a starting point
-x = reco_space.zero()
-#x = 0.5*phantom
-
-# define callback 
-callback = (odl.solvers.CallbackPrintIteration(step=200) &
-            odl.solvers.CallbackShow(step=200))
-# Run the algorithm
-odl.solvers.pdhg(x, f, g, op, tau=tau, sigma=sigma, niter=niter,
-                 callback=callback)
+        saveto_path = reco_path+'_step_par='+str(step_param)+'_reg_par='+str(reg_param)+'/iterate_{}'
+        
+        callback = odl.solvers.CallbackSaveToDisk(saveto=saveto_path,
+                                                  step=200, impl='numpy')
+    
+        
+        
+        # Initialize gradient operator
+        gradient = odl.Gradient(reco_space)
+        
+        # Column vector of two operators
+        op = odl.BroadcastOperator(forward_op, gradient)
+        
+        # Do not use the g functional, set it to zero.
+        g = odl.solvers.IndicatorNonnegativity(reco_space)
+        
+        # Create functionals for the dual variable
+        
+        # l2-squared data matching
+        l2_norm = odl.solvers.L2NormSquared(forward_op.range).translated(data)
+        
+        #
+        #reg_param = 0.015
+        
+        # Isotropic TV-regularization i.e. the l1-norm
+        l1_norm = reg_param * odl.solvers.GroupL1Norm(gradient.range)
+        
+        # Combine functionals, order must correspond to the operator K
+        f = odl.solvers.SeparableSum(l2_norm, l1_norm)
+        
+        # --- Select solver parameters and solve using PDHG --- #
+        
+        # Estimated operator norm, add 10 percent to ensure ||K||_2^2 * sigma * tau < 1
+        op_norm = 1.1 * 0.067 # 1.1 * odl.power_method_opnorm(forward_op.derivative(reco_space.zero()))
+        
+        
+        niter = 1001  # Number of iterations
+        tau = step_param / op_norm  # Step size for the primal variable
+        sigma = step_param / op_norm  # Step size for the dual variable
+        
+        
+        
+        # Choose a starting point
+        x = reco_space.zero()
+        #x = 0.5*phantom
+        
+        # Run the algorithm
+        odl.solvers.pdhg(x, f, g, op, tau=tau, sigma=sigma, niter=niter,
+                         callback=callback)
 
 
--- odl/contrib/etomo/examples/simulated/balls/pdhg_tv_lin/balls_no_noise.py
+++ odl/contrib/etomo/examples/simulated/balls/pdhg_tv_lin/balls_no_noise.py
@@ -166,7 +166,7 @@ f = odl.solvers.SeparableSum(l2_norm, l1_norm)
 # --- Select solver parameters and solve using PDHG --- #
 
 # Estimated operator norm, add 10 percent to ensure ||K||_2^2 * sigma * tau < 1
-op_norm = 1.1 * 0.067 # 1.1 * odl.power_method_opnorm(forward_op.derivative(reco_space.one()))
+op_norm = 1.1 * 0.067 # 1.1 * odl.power_method_opnorm(forward_op.derivative(reco_space.zero()))
 
 
 niter = 10000  # Number of iterations
